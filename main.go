51fc6ae161ab8c71743929f341bfc569bad5d71c
rishabh21g <biasedengineer@gmail.com>
2025-04-05 03:54:06 -0400

    variable

--- main.go
+++ main.go
@@ -2,7 +2,13 @@ package main
 
 import "fmt"
 
+func sum(a int, b int) int {
+	return a + b
+}
 func main() {
-    fmt.Println("Hello, World!")
-	
+	fmt.Println("Hello, World!")
+	// result := sum(120, 120)
+	var result int = sum(11, 4)
+	fmt.Println(result)
+
 }
