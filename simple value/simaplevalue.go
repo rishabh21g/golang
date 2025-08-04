ef247935bdaabb5d0449f3da4e3aaa94f69b5f73
rishabh21g <biasedengineer@gmail.com>
2025-08-04 11:50:11 +0530

    data types

---
+++ simple value/simaplevalue.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main() {
+	//simple value
+
+	//int
+	fmt.Println(1 + 1)
+
+	fmt.Println("Hello Golang")
+
+	//boolean
+	fmt.Println(true)
+
+	//floats
+
+	fmt.Println(10.636)
+
+	//operator
+
+	fmt.Println(7.0 / 3.0)
+}
